328927cedeedd26af26639348c6a905a59851665
silviachaluisa <veronica-14-1998@hotmail.com>
2024-01-23 09:21:55 -0500

    Ultimo Cambio

--- Form1.java
+++ Form1.java
@@ -1,8 +1,8 @@
 import javax.swing.*;
 
 public class Form1 {
-    private JPanel panel1;
-    private JTextField textField1;
     private JTextField textField2;
     private JButton ingresarButton;
+    private JTextField textField1;
+    JPanel Menu;
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
+import javax.swing.*;
+
 public class Main {
     public static void main (String[] args){
+
+        JFrame frame=new JFrame("Hola");
+        frame.setContentPane(new Form1().Menu);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+
+        frame.setVisible(true);//Muestro todo
 
     }
 }
