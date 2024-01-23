ffebbfe9a7c4afaeebd402a86b263f5d7c92c985
silviachaluisa <veronica-14-1998@hotmail.com>
2024-01-23 08:40:00 -0500

    Botones

--- Form1.java
+++ Form1.java
@@ -1,2 +1,8 @@
+import javax.swing.*;
+
 public class Form1 {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton ingresarButton;
 }
